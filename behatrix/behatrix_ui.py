dcb74e329d98d1c1cbc608970ab6860bebec18f7
Olivier Friard <olivier.friard@unito.it>
2019-09-11 09:36:35 +0200

    some work

--- behatrix/behatrix_ui.py
+++ behatrix/behatrix_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'behatrix/behatrix.ui'
+# Form implementation generated from reading ui file 'behatrix.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
