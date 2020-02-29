5837952f6ec04488941a2bd72cd1324692afa796
Olivier Friard <olivier.friard@unito.it>
2020-02-29 23:20:33 +0100

    cleaning

--- behatrix/behatrix_gui.py
+++ behatrix/behatrix_gui.py
@@ -436,7 +436,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 # test if viz.js and nodejs found
                 # script directory
                 print("sys.argv[0]", sys.argv[0])
-                print("sys.path[0]", sys.path[0])
+                print("sys.path", sys.path)
 
                 if pathlib.Path(sys.path[0]).is_file():  # frozen (pyinstaller)
                     syspath = pathlib.Path(sys.path[0]).parent
